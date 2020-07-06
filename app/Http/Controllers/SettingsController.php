fd8f708ffc69d3de16c82fb02ed8945d13410cde
Dries Vints <dries@vints.io>
2020-07-06 21:29:16 +0200

    Update settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,12 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+Route::view('/', 'welcome')->name('welcome');
 
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
     Route::view('/home', 'home')->name('home');
-    Route::get('/user/settings', 'User\\SettingsController@show')->name('user.settings');
-    Route::put('/user/settings', 'User\\SettingsController@update');
+    Route::get('/user/settings', 'SettingsController@show')->name('user.settings');
+    Route::put('/user/settings', 'SettingsController@update');
 });
