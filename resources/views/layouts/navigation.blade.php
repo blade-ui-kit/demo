9af3f087e838bbfa66aad738735196290269367b
Dries Vints <dries@vints.io>
2020-08-08 17:41:14 +0200

    Implement dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
             <div class="flex">
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-heroicon-o-globe class="h-8 w-auto text-indigo-600"/>
+                        <x-heroicon-o-globe class="h-8 w-auto text-indigo-600" />
                     </a>
                 </div>
 
@@ -19,22 +19,13 @@
             </div>
             <div class="hidden sm:ml-6 sm:flex sm:items-center">
                 <!-- Profile dropdown -->
-                <div class="ml-3 relative">
-                    <div>
+                <x-dropdown class="ml-3 relative">
+                    <x-slot name="trigger">
                         <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out" id="user-menu" aria-label="User menu" aria-haspopup="true">
                             <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                         </button>
-                    </div>
-                    <!--
-                      Profile dropdown panel, show/hide based on dropdown state.
+                    </x-slot>
 
-                      Entering: "transition ease-out duration-200"
-                        From: "transform opacity-0 scale-95"
-                        To: "transform opacity-100 scale-100"
-                      Leaving: "transition ease-in duration-75"
-                        From: "transform opacity-100 scale-100"
-                        To: "transform opacity-0 scale-95"
-                    -->
                     <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
                         <div class="py-1 rounded-md bg-white shadow-xs">
                             <a href="#" class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
@@ -46,7 +37,7 @@
                             <x-logout class="block text-left w-full px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"/>
                         </div>
                     </div>
-                </div>
+                </x-dropdown>
             </div>
             <div class="-mr-2 flex items-center sm:hidden">
                 <!-- Mobile menu button -->
