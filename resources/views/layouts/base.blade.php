f20648c48949454618614f5a99c6afd81c88e86d
Dries Vints <dries@vints.io>
2020-08-09 17:23:04 +0200

    Implement OG component

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,5 +1,10 @@
 <x-html class="bg-white h-screen antialiased leading-none">
     <x-slot name="head">
+        <x-og
+            title="{{ $component->title() }}"
+            description="Curate your bucket list and keep track of your next trips. Search for the most popular destinations on our planet."
+        />
+
         <script src="{{ asset('js/app.js') }}" defer></script>
 
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
