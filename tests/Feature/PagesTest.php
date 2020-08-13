2677157e4f37586ee4472d5b66080d5462c7507f
Dries Vints <dries@vints.io>
2020-08-13 22:14:18 +0200

    Implement tests

---
+++ tests/Feature/PagesTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PagesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function users_can_visit_the_homepage()
+    {
+        $this->get('/')
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function users_need_to_login()
+    {
+        $this->get('/home')
+            ->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function users_can_visit_their_dashboard()
+    {
+        $this->be(factory(User::class)->create());
+
+        $this->get('/home')
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function users_can_visit_their_settings()
+    {
+        $this->be(factory(User::class)->create());
+
+        $this->get('/settings')
+            ->assertStatus(200);
+    }
+}
