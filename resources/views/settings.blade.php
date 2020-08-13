0e6e3cdfd81f76cc68ce4c5f0c1f4e212fcff9c1
Dries Vints <dries@vints.io>
2020-08-13 22:08:10 +0200

    Add placeholders

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -23,17 +23,17 @@
                                     <div class="grid grid-cols-6 gap-6">
                                         <div class="col-span-6 sm:col-span-3">
                                             <x-label for="first_name" class="block text-sm font-medium leading-5 text-gray-700"/>
-                                            <x-input name="first_name" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            <x-input name="first_name" placeholder="John" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                         </div>
 
                                         <div class="col-span-6 sm:col-span-3">
                                             <x-label for="last_name" class="block text-sm font-medium leading-5 text-gray-700"/>
-                                            <x-input name="last_name" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            <x-input name="last_name" placeholder="Doe" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                         </div>
 
                                         <div class="col-span-6 sm:col-span-4">
                                             <x-label for="email_address" class="block text-sm font-medium leading-5 text-gray-700"/>
-                                            <x-input name="email_address" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            <x-input name="email_address" placeholder="john@example.com" class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                         </div>
 
                                         <div class="col-span-6 sm:col-span-3">
